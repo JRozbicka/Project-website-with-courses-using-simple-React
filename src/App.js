2e7b442aec9cb78a05b7ec6daf96c49b6a6bdeb5
JRozbicka <strawberka17@gmail.com>
2023-06-27 10:18:53 +0100

    start project with react tempate

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const App = ()=>{
+    return(
+        <div>
+        
+          Project website with courses simple React
+        </div>
+    )
+}
+export default App
