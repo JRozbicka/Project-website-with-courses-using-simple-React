7747986c9301adb8381016806e1fb0ffb3385396
JRozbicka <strawberka17@gmail.com>
2023-06-28 14:19:51 +0100

    done all state aplication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from "react";
 
-export const App = ()=>{
+export class App extends React.Component{
+  state= {
+    // global state
+    isLoading: false,
+    hasError: false,
+    errorMessage:'',
+    isInfoDisplayed: false,
+    infoMessage:' ',
+    // user state
+    isUserLoggedIn:'',
+    userDisplayName:'',
+    userEmail:'',
+    userAvatar:'',
+    // router state
+    notLoginUserRoute: 'Login', // newAccount or forgot password
+    //login page state
+    loginEmail:'',
+    loginPassword: '',
+    // create account page
+    createAccountEmail: '',
+    createAccountPassword: '',
+    createAccountRepeatPassword: '',
+    // recover password
+    recoverPasswordEmail: '',
+    // course list page
+    courses:null,
+    searchPhrases:''
+  }
+  render(){
+
+  
     return(
         <div>
         
           Project website with courses simple React!
           <h1>prickled</h1>
+          <h1>test2</h1>
         </div>
     )
+}
 }
 export default App
