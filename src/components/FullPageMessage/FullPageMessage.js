58da883e5094d70d7f92ad61f7cf8e039de68bef
JRozbicka <strawberka17@gmail.com>
2023-07-20 11:58:12 +0100

    fullpagemessage updated

--- src/components/FullPageMessage/FullPageMessage.js
+++ src/components/FullPageMessage/FullPageMessage.js
@@ -3,11 +3,14 @@ import Button from '../Button'
 import classes from './styles.module.css'
 import PropTypes from 'prop-types'
 import Typography from '../Typography'
+import InfoIcon from './InfoIcon'
+import ErrorIcon from './ErrorIcon'
 export const  FullPageMessage = (props) =>{
     const {
         className,
         buttonLabel='GO BACK',
         message,
+        IconVariant='info',
         ...otherProps
     } =props
     return (
@@ -15,6 +18,15 @@ export const  FullPageMessage = (props) =>{
             className= {`${classes.root}${className ? `${className}` : '' }`}
         {otherProps}
         <div className={classes.wrapper}>
+          {
+            IconVariant === 'info' ?
+            <InfoIcon/>
+            :
+            IconVariant === 'error' ?
+            <ErrorIcon/>
+            :
+            null
+          }
 
       <Typography
       className={classes.message}
@@ -34,6 +46,7 @@ export const  FullPageMessage = (props) =>{
 FullPageMessage.prototype ={
     className:PropTypes.string,
     message:PropTypes.string.isRequired,
-    buttonLabel:PropTypes.string
+    buttonLabel:PropTypes.string,
+    IconVariant:PropTypes.oneOf(['info','error'])
 }
 export default FullPageMessage
