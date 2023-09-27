2e04d00412542814640d6f31bd3cf62ab6b16ebd
JRozbicka <strawberka17@gmail.com>
2023-09-27 11:58:39 +0100

    loginForm component filled with content

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -2,6 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Logo from '../Logo'
 import classes from './styles.module.css'
+import Typography from '../Typography'
+import TextField from '../TextField/TextField'
+import Button from '../Button'
 
 export const LoginForm = (props) => {
   const {
@@ -14,7 +17,39 @@ export const LoginForm = (props) => {
       className={`${classes.root}${className ? ` ${className}` : ''}`}
       {...otherProps}
     >
-      <Logo/>
+      <Logo
+      className={classes.logo}/>
+      <Typography 
+      className={classes.header}
+      variant={'h1'}
+      >
+log in 👋
+        </Typography>
+        <TextField 
+        className={classes.textField}
+        placeholder={'Email'} />
+        <TextField 
+        className={classes.textField}
+        placeholder = {'password'} />
+        <Button 
+        className={classes.button}
+        variant={'contained'}
+        color={'primary'}>
+          Login
+        </Button>
+        <Button 
+        className={classes.button}
+        variant={'contained'}
+        color={'secondary'}>
+          Crete Account
+        </Button>
+        <Button 
+        className={classes.button}
+        variant={'text'}
+        color={'primary'}>
+          Forgotten password
+        </Button>
+       
     </div>
   )
 }
--- src/components/Typography/Typography.js
+++ src/components/Typography/Typography.js
@@ -1,29 +1,32 @@
-import React from "react";
-import classes from './styles.module.css'
+import React from 'react'
 import PropTypes from 'prop-types'
-console.log(classes);
-export const Typography = (props) =>{
-    const {
-        className,
-        children,
-        variant,
-        ...otherProps
-    } = props
-    const variantClasses= classes[variant]
-   
-    return(
-        <span
-            className = {`${classes.root}${className ? ` ${className}` : ''}
-            ${variantClasses ? ` ${variantClasses}` : ''}`}
-            {...otherProps}
-            >
-         {children}
-        </span>
-    )
+
+import classes from './styles.module.css'
+
+export const Typography = (props) => {
+  const {
+    className,
+    children,
+    variant,
+    ...otherProps
+  } = props
+
+  const variantClass = classes[variant]
+
+  return (
+    <span
+      className={`${classes.root}${className ? ` ${className}` : ''}${variantClass ? ` ${variantClass}` : ''}`}
+      {...otherProps}
+    >
+      {children}
+    </span>
+  )
 }
-Typography.prototype={
-    className: PropTypes.string,
-    children: PropTypes.node,
-    variant: PropTypes.oneOf(['h1','h3','button'])
+
+Typography.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+  variant: PropTypes.oneOf(['h1', 'h3', 'button'])
 }
+
 export default Typography
