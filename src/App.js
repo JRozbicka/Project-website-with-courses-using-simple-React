6d196eae462cb1a5573c15282f2a0a0fa1b7794a
JRozbicka <strawberka17@gmail.com>
2023-10-11 13:14:29 +0100

    between sides add function

--- src/App.js
+++ src/App.js
@@ -1,151 +1,129 @@
 import React from 'react'
-
 import FullPageLayout from './components/FullPageLayout'
 import FullPageMessage from './components/FullPageMessage'
 import FullPageLoader from './components/FullPageLoader'
 import Message from './components/Message'
 import LoginForm from './components/LoginForm'
-import CreateAccountForm from './components/CreateAccountForm/CreateAccountForm'
-import RecoverPasswordForm from './components/RecoverPasswordForm/RecoverPasswordForm'
-
+import CreateAccountForm from './components/CreateAccountForm'
+import RecoverPasswordForm from './components/RecoverPasswordForm'
 export class App extends React.Component {
-  state = {
-    // global state
-    isLoading: false,
-    hasError: false,
-    errorMessage: '',
-    isInfoDisplayed: false,
-    infoMessage: '',
-
-    // user/auth state
-    isUserLoggedIn: false,
-    userDisplayName: '',
-    userEmail: '',
-    userAvatar: '',
-
-    // router state
-    notLoginUserRoute: 'RECOVER-PASSWORD', // 'CREATE-ACCOUNT' or 'RECOVER-PASSWORD'
-
-    // login page state
-    loginEmail: '',
-    loginPassword: '',
-
-    // create account page
-    createAccountEmail: '',
-    createAccountPassword: '',
-    createAccountRepeatPassword: '',
-
-    // recover password page
-    recoverPasswordEmail: '',
-
-    // course list page
-    courses: null,
-    searchPhrase: ''
-  }
-
-  render () {
-    const {
-      loginEmail,
-      loginPassword,
-      isLoading,
-      isInfoDisplayed,
-      infoMessage,
-      hasError,
-      errorMessage,
-      notLoginUserRoute,
-      createAccountEmail,
-      createAccountPassword,
-      createAccountRepeatPassword,
-      recoverPasswordEmail
-    } = this.state
-
-    return (
-      <div>
-
-        {
-          notLoginUserRoute === 'LOGIN' ?
-            <FullPageLayout>
-              <LoginForm
-                email={loginEmail}
-                password={loginPassword}
-                onChangeEmail={(e) => this.setState(() => ({ loginEmail: e.target.value }))}
-                onChangePassword={(e) => this.setState(() => ({ loginPassword: e.target.value }))}
-                onClickLogin={() => console.log('onClickLogin')}
-                onClickCreateAccount={() => console.log('onClickCreateAccount')}
-                onClickForgotPassword={() => console.log('onClickForgotPassword')}
-              />
-            </FullPageLayout>
-            :
-            notLoginUserRoute === 'CREATE-ACCOUNT' ?
-            <FullPageLayout>
-              <CreateAccountForm
-              email={createAccountEmail}
-              onChangeEmail={(e) => this.setState(() => ({ createAccountEmail: e.target.value }))}
-              password={createAccountPassword}
-              onChangePassword={(e) => this.setState(() => ({ createAccountPassword: e.target.value }))}
-              reapedPassword={createAccountRepeatPassword}
-              onChangeReapedPassword={(e) => this.setState(() => ({ createAccountRepeatPassword: e.target.value }))}
-              onClickCreateAccount={() => console.log('onClickCreateAccount')}
-              onClickBackToLogin={() => console.log('onClickBackToLogin')}
-            />
-              
-            </FullPageLayout>
-            :
-            notLoginUserRoute
-            === 'RECOVER=PASSWORD' ?
-          <FullPageLayout>
-
-         
-
-            <RecoverPasswordForm 
-            email={recoverPasswordEmail}
-            onChangeEmail={(e) => this.setState(()=>({recoverPasswordEmail: e.target.value}))}
-            onClickRecover={() => console.log('onClickRecover')}
-              onClickBackToLogin={() => console.log('onClickBackToLogin')}
-            />
-            
-             </FullPageLayout>
-            :
-            null
-        }
-
-        {
-          isLoading ?
-            <FullPageLoader />
-            :
-            null
-        }
-
-        {
-          isInfoDisplayed ?
-            <FullPageMessage
-              message={infoMessage}
-              iconVariant={'info'}
-              onButtonClick={console.log}
-            />
-            :
-            null
-        }
-
-        {
-          hasError ?
-            <FullPageLayout
-              className={'wrapper-class'}
-            >
-              <Message
-                className={'regular-class'}
-                message={errorMessage}
-                iconVariant={'error'}
-                onButtonClick={console.log}
-              />
-            </FullPageLayout>
-            :
-            null
-        }
-
-      </div>
-    )
-  }
+state = {
+// global state
+isLoading: false,
+hasError: false,
+errorMessage: '',
+isInfoDisplayed: false,
+infoMessage: '',
+// user/auth state
+isUserLoggedIn: false,
+userDisplayName: '',
+userEmail: '',
+userAvatar: '',
+// router state
+notLoginUserRoute: 'LOGIN', // 'CREATE-ACCOUNT' or 'RECOVER-PASSWORD'
+// login page state
+loginEmail: '',
+loginPassword: '',
+// create account page
+createAccountEmail: '',
+createAccountPassword: '',
+createAccountRepeatPassword: '',
+// recover password page
+recoverPasswordEmail: '',
+// course list page
+courses: null,
+searchPhrase: ''
 }
-
+render () {
+const {
+loginEmail,
+loginPassword,
+isLoading,
+isInfoDisplayed,
+infoMessage,
+hasError,
+errorMessage,
+notLoginUserRoute,
+createAccountEmail,
+createAccountPassword,
+createAccountRepeatPassword,
+recoverPasswordEmail
+} = this.state
+return (
+<div>
+{
+notLoginUserRoute === 'LOGIN' ?
+<FullPageLayout>
+<LoginForm
+email={loginEmail}
+password={loginPassword}
+onChangeEmail={(e) => this.setState(() => ({ loginEmail: e.target.value }))}
+onChangePassword={(e) => this.setState(() => ({ loginPassword: e.target.value }))}
+onClickLogin={() => console.log('onClickLogin')}
+onClickCreateAccount={() => this.setState(() => ({ notLoginUserRoute: 'CREATE-ACCOUNT' }))}
+onClickForgotPassword={() => this.setState(() => ({ notLoginUserRoute: 'RECOVER-PASSWORD' }))}
+/>
+</FullPageLayout>
+:
+notLoginUserRoute === 'CREATE-ACCOUNT' ?
+<FullPageLayout>
+<CreateAccountForm
+email={createAccountEmail}
+password={createAccountPassword}
+repeatPassword={createAccountRepeatPassword}
+onChangeEmail={(e) => this.setState(() => ({ createAccountEmail: e.target.value }))}
+onChangePassword={(e) => this.setState(() => ({ createAccountPassword: e.target.value }))}
+onChangeRepeatPassword={(e) => this.setState(() => ({ createAccountRepeatPassword: e.target.value }))}
+onClickCreateAccount={() => console.log('onClickCreateAccount')}
+onClickBackToLogin={() => this.setState(() => ({ notLoginUserRoute: 'LOGIN' }))}
+/>
+</FullPageLayout>
+:
+notLoginUserRoute === 'RECOVER-PASSWORD' ?
+<FullPageLayout>
+<RecoverPasswordForm
+email={recoverPasswordEmail}
+onChangeEmail={(e) => this.setState(() => ({ recoverPasswordEmail: e.target.value }))}
+onClickRecover={() => console.log('onClickRecover')}
+onClickBackToLogin={() => this.setState(() => ({ notLoginUserRoute: 'LOGIN' }))}
+/>
+</FullPageLayout>
+:
+null
+}
+{
+isLoading ?
+<FullPageLoader />
+:
+null
+}
+{
+isInfoDisplayed ?
+<FullPageMessage
+message={infoMessage}
+iconVariant={'info'}
+onButtonClick={console.log}
+/>
+:
+null
+}
+{
+hasError ?
+<FullPageLayout
+className={'wrapper-class'}
+>
+<Message
+className={'regular-class'}
+message={errorMessage}
+iconVariant={'error'}
+onButtonClick={console.log}
+/>
+</FullPageLayout>
+:
+null
+}
+</div>
+)
+}
+}
 export default App
